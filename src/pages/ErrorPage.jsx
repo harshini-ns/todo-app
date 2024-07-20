b3be3f70b3f97d6f271f4dbc54833120242f6ae3
harshini-ns <harshininaveen9801@gmail.com>
2024-07-20 15:03:43 +0530

    initial commit

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+
+export default function ErrorPage() {
+    return (
+        <Container>
+            <h1 className="my-3" > Ooopsss!!!</h1>
+            <p> Page not found </p>
+        </Container>
+
+    );
+}
+
